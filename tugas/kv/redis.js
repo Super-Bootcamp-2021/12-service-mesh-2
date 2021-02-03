25102213b5a83b04b3c614b329203fd842b43ea0
rexus76 <annas.adharuqudni@gmail.com>
2021-02-03 16:34:56 +0700

    add getter setter value KV

--- tugas/kv/redis.js
+++ tugas/kv/redis.js
@@ -4,25 +4,40 @@ const client = redis.createClient();
 
 const getAsync = promisify(client.get).bind(client);
 const setAsync = promisify(client.set).bind(client);
-const delAsync = promisify(client.del).bind(client);
+// const delAsync = promisify(client.del).bind(client);
 
 client.on('error', (error) => {
   console.error(error);
   client.end(true);
 });
 
-client.on('connect', () => {
-  main();
-});
-
-async function main() {
+async function setValue(fieldname, value) {
   try {
-    await setAsync('name', 'budiman');
-    const val = await getAsync('name');
-    console.log(val);
-    await delAsync('name');
-    client.end(true);
+    let store = await getAsync(fieldname);
+    let parsing = '';
+    let newValue = '';
+    if (store == null) {
+      newValue = `${value}`;
+    } else {
+      parsing = JSON.parse(store);
+      newValue = `${parsing},${value}`;
+    }
+    await setAsync(fieldname, JSON.stringify(newValue));
   } catch (err) {
     console.error(err);
   }
 }
+async function getValue(fieldname) {
+  try {
+    let store = await getAsync(fieldname);
+    const result = JSON.parse(store).split(',');
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+module.exports = {
+  getValue,
+  setValue,
+};
