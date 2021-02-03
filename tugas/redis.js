38d3e6cea145a95b7411288c3df90fb90ba601d8
fauzana18 <fauzana18@gmail.com>
2021-02-03 17:53:04 +0700

    buat file redis terpisah

---
+++ tugas/redis.js
@@ -0,0 +1,15 @@
+const redis = require('redis');
+const { promisify } = require('util');
+const client = redis.createClient();
+
+const startRedis = promisify(client.on).bind(client);
+const getData = promisify(client.get).bind(client);
+const setData = promisify(client.set).bind(client);
+const delData = promisify(client.del).bind(client);
+
+module.exports = {
+  startRedis,
+  setData,
+  getData,
+  delData,
+};
