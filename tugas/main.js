a876b4860ad6c04e9517d401cc45f258695bb4a1
fauzana18 <fauzana18@gmail.com>
2021-02-04 08:40:52 +0700

    revisi tasks service sedikit

--- tugas/main.js
+++ tugas/main.js
@@ -2,8 +2,8 @@ const { startRedis } = require('./redis');
 const { serve } = require('./server');
 
 async function main() {
-    await startRedis('connect');
-    serve();
+  await startRedis('connect');
+  serve();
 }
 
 main();
--- tugas/redis.js
+++ tugas/redis.js
@@ -7,14 +7,9 @@ const getData = promisify(client.get).bind(client);
 const setData = promisify(client.set).bind(client);
 const delData = promisify(client.del).bind(client);
 
-client.on('connect', () => {
-    console.log('set datanya : ', setData)
-})
-
-
 module.exports = {
-    startRedis,
-    setData,
-    getData,
-    delData,
+  startRedis,
+  setData,
+  getData,
+  delData,
 };
